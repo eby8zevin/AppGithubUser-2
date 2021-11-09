47f90473e857a5cbe60662c03a98ab4d1404c867
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-11-10 00:00:46 +0700

    Java ApiService.java

--- app/src/main/java/com/ahmadabuhasan/appgithubuser/networking/ApiService.java
+++ app/src/main/java/com/ahmadabuhasan/appgithubuser/networking/ApiService.java
@@ -4,20 +4,17 @@ import com.ahmadabuhasan.appgithubuser.model.Search;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Headers;
+import retrofit2.http.Header;
 import retrofit2.http.Query;
 
 public interface ApiService {
 
-    /*@GET("users/{username}")
-    Call<ModelUser> detailUser(@Path("username") String username);*/
-
-    @Headers({"Authorization: token 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"})
+    /*@Headers({"Authorization: token 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"})
     @GET("/search/users")
     Call<Search> searchUser(
-            @Query("q") String username);
+            @Query("q") String username);*/
 
-    /*@GET("users/{username}/followers")
-    Call<List<ModelFollow>> followersUser(@Headers("Authorization") String authorization,
-                                          @Path("username") String username);*/
+    @GET("/search/users")
+    Call<Search> searchUser(@Header("Authorization") String authorization,
+                            @Query("q") String username);
 }
