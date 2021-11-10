fdbf99b9a3ecf3125c39bbccc494de4cc5226e51
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-11-10 23:03:45 +0700

    Java ApiService.java

--- app/src/main/java/com/ahmadabuhasan/appgithubuser/networking/ApiService.java
+++ app/src/main/java/com/ahmadabuhasan/appgithubuser/networking/ApiService.java
@@ -1,20 +1,25 @@
 package com.ahmadabuhasan.appgithubuser.networking;
 
 import com.ahmadabuhasan.appgithubuser.model.Search;
+import com.ahmadabuhasan.appgithubuser.model.User;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiService {
 
-    /*@Headers({"Authorization: token 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"})
+    /*@Headers({"Authorization: token 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"})*/
     @GET("/search/users")
     Call<Search> searchUser(
-            @Query("q") String username);*/
+            @Query("q") String username);
 
-    @GET("/search/users")
+   /* @GET("/search/users")
     Call<Search> searchUser(@Header("Authorization") String authorization,
-                            @Query("q") String username);
+                            @Query("q") String username);*/
+
+    @GET("users/{username}")
+    Call<User> detailUser(
+            @Path("username") String username);
 }
