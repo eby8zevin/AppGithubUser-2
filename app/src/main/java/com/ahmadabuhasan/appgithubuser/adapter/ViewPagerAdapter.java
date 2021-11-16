f7503470501ea33434f785c48a6a23c47213333a
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-11-17 00:55:07 +0700

    Java ViewPagerAdapter.java

---
+++ app/src/main/java/com/ahmadabuhasan/appgithubuser/adapter/ViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.ahmadabuhasan.appgithubuser.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.ahmadabuhasan.appgithubuser.ui.FollowersFragment;
+import com.ahmadabuhasan.appgithubuser.ui.FollowingFragment;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    public ViewPagerAdapter(AppCompatActivity activity) {
+        super(activity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new FollowersFragment();
+                break;
+            case 1:
+                fragment = new FollowingFragment();
+                break;
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
