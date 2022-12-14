faee9df6f1f43c9b318fee80109d25a9e0649527
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-12-14 12:41:31 +0700

    Api Service

---
+++ Submission-Kotlin/app/src/main/java/com/ahmadabuhasan/appgithubuser/api/ApiService.kt
@@ -0,0 +1,33 @@
+package com.ahmadabuhasan.appgithubuser.api
+
+import com.ahmadabuhasan.appgithubuser.model.Items
+import com.ahmadabuhasan.appgithubuser.model.ResponseDetailUser
+import com.ahmadabuhasan.appgithubuser.model.ResponseFollow
+import com.ahmadabuhasan.appgithubuser.model.ResponseSearch
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("search/users")
+    fun search(
+        @Query("q") username: String
+    ): Call<ResponseSearch>
+
+    @GET("users/{username}")
+    fun detailUser(
+        @Path("username") username: String
+    ): Call<ResponseDetailUser>
+
+    @GET("users/{username}/followers")
+    fun followers(
+        @Path("username") username: String
+    ): Call<ArrayList<ResponseFollow>>
+
+    @GET("users/{username}/following")
+    fun following(
+        @Path("username") username: String
+    ): Call<ArrayList<ResponseFollow>>
+}
