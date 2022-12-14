09e77cda790e0c789d7859d6f71cc69fdc397369
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-12-14 12:40:22 +0700

    Add viewModel & viewPager2

--- Submission-Kotlin/app/src/main/java/com/ahmadabuhasan/appgithubuser/ui/UserDetailActivity.kt
+++ Submission-Kotlin/app/src/main/java/com/ahmadabuhasan/appgithubuser/ui/UserDetailActivity.kt
@@ -2,21 +2,22 @@ package com.ahmadabuhasan.appgithubuser.ui
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import androidx.activity.viewModels
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.ahmadabuhasan.appgithubuser.R
+import com.ahmadabuhasan.appgithubuser.adapter.SectionsPagerAdapter
 import com.ahmadabuhasan.appgithubuser.databinding.ActivityUserDetailBinding
-import com.ahmadabuhasan.appgithubuser.model.UserResponse
+import com.ahmadabuhasan.appgithubuser.viewmodel.MainViewModel
 import com.bumptech.glide.Glide
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class UserDetailActivity : AppCompatActivity() {
 
-    companion object {
-        const val EXTRA_USER = "extra_user"
-    }
-    /*
-    Penulisan companion object sebaiknya diletakkan di paling bawah dari sebuah class sesuai dengan konvensi penulisan kode Kotlin di tautan https://kotlinlang.org/docs/reference/coding-conventions.html#class-layout.
-     */
-
+    private val viewModel: MainViewModel by viewModels()
     private lateinit var binding: ActivityUserDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,31 +25,38 @@ class UserDetailActivity : AppCompatActivity() {
         binding = ActivityUserDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportActionBar?.title = "Detail User"
-        /*
-        Hindari penggunaan hardcoded string. Kamu bisa memindahkannya kedalam file strings.xml.
-         */
+        supportActionBar?.title = resources.getString(R.string.detail_user)
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
 
-        val user = intent.getParcelableExtra<UserResponse>(EXTRA_USER)
-        if (user != null) {
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        val viewPager: ViewPager2 = binding.viewPager
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = binding.tabs
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+
+        username = intent.getStringExtra(EXTRA_USER).toString()
+        showViewModel()
+        viewModel.getIsLoading.observe(this, this::showLoading)
+    }
+
+    private fun showViewModel() {
+        viewModel.detailUser(username)
+        viewModel.getUserDetail.observe(this) { detailUser ->
             Glide.with(this)
-                .load(user.avatar)
+                .load(detailUser.avatar_url)
                 .skipMemoryCache(true)
                 .into(binding.imgAvatar)
 
-            binding.tvName.text = user.name
-            binding.tvUsername.text = buildString {
-                append("@ ")
-                append(user.username)
-            }
-            binding.tvCompany.text = user.company
-            binding.tvLocation.text = user.location
-            binding.tvRepositoryValue.text = user.repository.toString()
-            binding.tvFollowersValue.text = user.follower.toString()
-            binding.tvFollowingValue.text = user.following.toString()
+            binding.tvName.text = detailUser.name
+            binding.tvUsername.text = detailUser.login
+            binding.tvCompany.text = detailUser.company
+            binding.tvLocation.text = detailUser.location
+            binding.tvRepositoryValue.text = detailUser.public_repos
+            binding.tvFollowersValue.text = detailUser.followers
+            binding.tvFollowingValue.text = detailUser.following
         }
     }
 
@@ -58,5 +66,24 @@ class UserDetailActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
+    }
+
+    companion object {
+        const val EXTRA_USER = "extra_user"
+        var username = String()
+
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.follower,
+            R.string.following
+        )
     }
 }
