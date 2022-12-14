2697f7e2d51dcf4ae0870a4a886b83bcbcb642ac
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-12-14 12:44:47 +0700

    Business Logic Followers

---
+++ Submission-Kotlin/app/src/main/java/com/ahmadabuhasan/appgithubuser/ui/FollowersFragment.kt
@@ -0,0 +1,81 @@
+package com.ahmadabuhasan.appgithubuser.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ahmadabuhasan.appgithubuser.adapter.FollowAdapter
+import com.ahmadabuhasan.appgithubuser.databinding.FragmentFollowersBinding
+import com.ahmadabuhasan.appgithubuser.model.ResponseFollow
+import com.ahmadabuhasan.appgithubuser.viewmodel.MainViewModel
+
+class FollowersFragment : Fragment() {
+
+    private val viewModel: MainViewModel by viewModels()
+    private val adapter = FollowAdapter()
+
+    private lateinit var binding: FragmentFollowersBinding
+    private val _binding get() = binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentFollowersBinding.inflate(inflater, container, false)
+        return _binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        showViewModel()
+        showRecyclerView()
+        viewModel.getIsLoading.observe(viewLifecycleOwner, this::showLoading)
+    }
+
+    private fun showViewModel() {
+        viewModel.followers(UserDetailActivity.username)
+        viewModel.getFollowers.observe(viewLifecycleOwner) { followers ->
+            if (followers.size != 0) {
+                adapter.setData(followers)
+            } else {
+                Toast.makeText(context, "Followers Not Found", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun showRecyclerView() {
+        binding.rvFollowers.layoutManager = LinearLayoutManager(activity)
+        binding.rvFollowers.setHasFixedSize(true)
+        binding.rvFollowers.adapter = adapter
+
+        adapter.setOnItemClickCallback { data -> selectedUser(data) }
+    }
+
+    private fun selectedUser(user: ResponseFollow) {
+        Toast.makeText(context, "You choose ${user.login}", Toast.LENGTH_SHORT).show()
+
+        val i = Intent(activity, UserDetailActivity::class.java)
+        i.putExtra(UserDetailActivity.EXTRA_USER, user.login)
+        startActivity(i)
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.followers(UserDetailActivity.username)
+    }
+}
